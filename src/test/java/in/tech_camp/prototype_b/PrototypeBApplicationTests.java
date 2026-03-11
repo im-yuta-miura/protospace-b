5424589f38ce84aef641016a3aff8afc0d2df7c3
im-yuta-miura <y.miura@infomart.co.jp>
2026-03-11 13:23:36 +0900

    プロジェクトの雛形を作るため、Spring Initializrでプロジェクトを新規作成

---
+++ src/test/java/in/tech_camp/prototype_b/PrototypeBApplicationTests.java
@@ -0,0 +1,13 @@
+package in.tech_camp.prototype_b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrototypeBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
