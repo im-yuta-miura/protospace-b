5778184f1fb13a8d0d4f712c4ba8da09f8b782f1
ishida-ryuga <r.ishida@infomart.co.jp>
2026-03-11 16:49:16 +0900

    COMMENTテーブル追加修正

--- src/main/resources/db/migration/V3__create_comments_table.sql
+++ src/main/resources/db/migration/V3__create_comments_table.sql
@@ -1,6 +1,9 @@
-CREATE TABLE IF NOT EXISTS comments(
-  comment_id  INT
-  content     
-  user_id
-  prototype_id
-)
+CREATE TABLE comments(
+  comment_id  SERIAL  NOT NULL,
+  content     TEXT  NOT NULL,
+  user_id  INT  NOT NULL,
+  prototype_id  INT  NOT NULL,
+  PRIMARY KEY (comment_id),
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (prototype_id) REFERENCES prototype(id) ON DELETE CASCADE
+);
