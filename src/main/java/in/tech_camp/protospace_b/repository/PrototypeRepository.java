f5c732477b8c48bcd326839bf0a6c581bd269e05
ishida-ryuga <r.ishida@infomart.co.jp>
2026-03-12 15:09:12 +0900

    リポジトリに保存機能追加

--- src/main/java/in/tech_camp/protospace_b/repository/PrototypeRepository.java
+++ src/main/java/in/tech_camp/protospace_b/repository/PrototypeRepository.java
@@ -2,7 +2,9 @@ package in.tech_camp.protospace_b.repository;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 
 import in.tech_camp.protospace_b.entity.PrototypeEntity;
@@ -12,4 +14,8 @@ public interface PrototypeRepository {
   
   @Select("SELECT * FROM prototypes")
     List<PrototypeEntity> findAll();
+
+  @Insert("INSERT INTO prototypes (title, catchphrase, concept, image) VALUES (#{title}, #{catchphrase}, #{concept}, #{image})")
+  @Options(useGeneratedKeys = true, keyProperty = "id")
+  void insert(PrototypeEntity prototype);
 }
