ac631036d9a393ac8fc655496c44e22847399316
Kaho-O <kaho.okuda@infomart.co.jp>
2026-03-12 10:08:30 +0900

    Protorype_リポジトリ、エンティティ
    Prototype

---
+++ src/main/java/in/tech_camp/protospace_b/entity/PrototypeEntity.java
@@ -0,0 +1,13 @@
+package in.tech_camp.protospace_b.entity;
+
+import lombok.Data;
+
+@Data
+public class PrototypeEntity {
+  private Integer id;
+  private String title;
+  private String catchphrase;
+  private String concept;
+  private String image;
+  private Integer user_id;
+}
---
+++ src/main/java/in/tech_camp/protospace_b/repository/PrototypeRepository.java
@@ -0,0 +1,15 @@
+package in.tech_camp.protospace_b.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import in.tech_camp.protospace_b.entity.PrototypeEntity;
+
+@Mapper
+public interface PrototypeRepository {
+  
+  @Select("SELECT * FROM prototypes")
+    List<PrototypeEntity> findAll();
+}
