d165abd6f744ea19fcbf2dfe1f73ea208d59be83
Kaho-O <kaho.okuda@infomart.co.jp>
2026-03-13 09:44:26 +0900

    プロトタイプ一覧表示_ログインユーザー表示
    ログインユーザー表示

--- src/main/java/in/tech_camp/protospace_b/controller/PrototypeController.java
+++ src/main/java/in/tech_camp/protospace_b/controller/PrototypeController.java
@@ -2,11 +2,13 @@ package in.tech_camp.protospace_b.controller;
 
 import java.util.List;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import in.tech_camp.protospace_b.entity.PrototypeEntity;
+import in.tech_camp.protospace_b.entity.UserEntity;
 import in.tech_camp.protospace_b.repository.PrototypeRepository;
 import lombok.AllArgsConstructor;
 
@@ -16,10 +18,13 @@ public class PrototypeController {
   private final PrototypeRepository prototypeRepository;
 
   @GetMapping("/")
-  public String showPrototype(Model model) {
-    String userName = "新井";
+  public String showPrototype(@AuthenticationPrincipal UserEntity user, Model model) {
+    if (user != null) {
+        model.addAttribute("name", user.getName());
+    } else {
+        model.addAttribute("name", "ゲスト");
+    }
     List<PrototypeEntity> prototypes = prototypeRepository.findAll();
-    model.addAttribute("userName", userName);
     model.addAttribute("prototypes",prototypes);
     return "prototypes/index";
   }
