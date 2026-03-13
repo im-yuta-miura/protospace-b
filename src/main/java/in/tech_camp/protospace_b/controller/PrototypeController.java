a8e9a3e316dd31705bf7dfa526b5cf9fc5ef74e4
snozawa-star <s.nozawa@infomart.co.jp>
2026-03-13 11:46:31 +0900

    プロトタイプ詳細機能の修正

--- src/main/java/in/tech_camp/protospace_b/controller/PrototypeController.java
+++ src/main/java/in/tech_camp/protospace_b/controller/PrototypeController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import in.tech_camp.protospace_b.entity.PrototypeEntity;
+import in.tech_camp.protospace_b.entity.UserEntity;
 import in.tech_camp.protospace_b.repository.PrototypeRepository;
+import in.tech_camp.protospace_b.repository.UserRepository;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
@@ -14,12 +16,15 @@ import lombok.AllArgsConstructor;
 public class PrototypeController {
 
   private final PrototypeRepository prototypeRepository;
-
+  private final UserRepository userRepository;
   
   @GetMapping("/prototypes/{prototypeId}")
-  public String showTweetDetail(@PathVariable("prototypeId") Integer prototypeId, Model model) {
+  public String showPrototypeDetail(@PathVariable("prototypeId") Integer prototypeId, Model model) {
       PrototypeEntity prototype = prototypeRepository.findById(prototypeId);
+      UserEntity user = userRepository.findById(prototype.getUser_id());
       model.addAttribute("prototype", prototype);
+      model.addAttribute("user", user);
+      
       return "prototypes/detail";
   }
   
