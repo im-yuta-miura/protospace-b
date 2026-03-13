c5431e303561a8fee774fba59c8b812461febaab
ishida-ryuga <r.ishida@infomart.co.jp>
2026-03-13 14:50:40 +0900

    画像の保存追加修正

---
+++ src/main/java/in/tech_camp/protospace_b/WebConfig.java
@@ -0,0 +1,16 @@
+package in.tech_camp.protospace_b;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/uploads/**")
+            .addResourceLocations("file:src/main/resources/static/uploads/");
+  }
+}
